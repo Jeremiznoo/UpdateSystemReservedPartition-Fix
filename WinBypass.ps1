c012c4ca55f22db7498a264c8a58f7146d7d1bab
Jérémy <93448655+J-iznoo@users.noreply.github.com>
2023-10-12 16:35:28 +0200

    Update WinBypass.ps1

--- WinBypass.ps1
+++ WinBypass.ps1
@@ -1,41 +1,41 @@
-﻿##______ Author : Jérémy Diaz______## 
+﻿##______ Author : J-iznoo______## 
 
 clear
 Write-Host ' '
-Write-Host 'Ce script permet de vider la partition réservée au système pour pouvoir installer Windows 11 sur un pc dont cette partition est pleine.'
+Write-Host 'This script empties the partition reserved for the system so that Windows 11 can be installed on a PC whose partition is full.'
 Write-Host ' '
 
-# Débute une boucle qui se executera le script uniquement si l'utilisateur répond oui, sinon le script sera annulé
+# Starts a loop that will execute the script only if the user answers yes, otherwise the script will be cancelled
 
 do {
     Write-Host ' '
-    $NAME = Read-Host -Prompt 'Pour continuer et exécuter le script, tapez oui, sinon le script sera annulé en tapant non'
-    if ($NAME -eq "oui") {
+    $NAME = Read-Host -Prompt 'To continue and execute the script, type yes, otherwise the script will be cancelled by typing no '
+    if ($NAME -eq "yes") {
         Write-Host ' '
-        Write-Host "Le script est en train de s'exécuter..."
+        Write-Host "The script is running..."
     }
-    elseif ($NAME -eq "non") {
+    elseif ($NAME -eq "no") {
         Write-Host ' '
-        Write-Host "Fr : Le script a été annulé par l'utilisateur"
+        Write-Host "The script has been cancelled by the user"
         Write-Host ' '
         return
     }
     else {
-        # Affiche un message d'erreur pour une réponse invalide
+        # Displays an error message for an invalid response
         Write-Host ' '
-        Write-Host "Réponse invalide. Veuillez répondre avec 'oui' ou 'non'."
+        Write-Host "Invalid answer. Please answer with 'yes' or 'no'."
     }
-} until ($NAME -eq "oui" -or $NAME -eq "non")
-# Fin de la boucle do-while
+} until ($NAME -eq "yes" -or $NAME -eq "no")
+# End of do-while loop
 
-# Obtient la politique d'exécution pour l'utilisateur courant
+# Gets the execution policy for the current user
 Get-ExecutionPolicy -Scope CurrentUser
 
-# Monte le lecteur Y: pour la partition réservée au système
+# Mount drive Y: for the system partition
 mountvol y: /s
 
-# Accède au répertoire Y:\EFI\Microsoft\BOOT
+# Accesses directory Y:\EFI\Microsoft\BOOT
 cd Y:\EFI\Microsoft\BOOT
 
-# Supprime le dossier "Fonts"
+# Deletes the "Fonts" folder
 del Fonts

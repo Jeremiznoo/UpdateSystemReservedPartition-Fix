19d6503e8cecb5c373061872e82fa6f4c28fcdb4
Jérémy <93448655+J-iznoo@users.noreply.github.com>
2023-10-12 15:22:31 +0200

    Run script.bat

--- Run script.bat
+++ Run script.bat
@@ -1,4 +1,4 @@
-Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList '-noexit -ExecutionPolicy Bypass -File %~dp0Win11Bypass.ps1' -Verb RunAs}"
+Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList '-noexit -ExecutionPolicy Bypass -File %~dp0WinBypass.ps1' -Verb RunAs}"
 
 
 # %~dp0 puis le nom du script 

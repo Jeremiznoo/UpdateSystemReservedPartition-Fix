fb35b6cb94f04586f12123b220a4b62d20cea39e
Jérémy <93448655+J-iznoo@users.noreply.github.com>
2023-10-12 15:23:23 +0200

    Run script.bat

--- Run script.bat
+++ Run script.bat
@@ -1,5 +1,3 @@
+# Don't forget to move the script and this runner to the desktop before execute 
+
 Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList '-noexit -ExecutionPolicy Bypass -File %~dp0WinBypass.ps1' -Verb RunAs}"
-
-
-# %~dp0 puis le nom du script 
-# -ExecutionPolicy Bypass 
